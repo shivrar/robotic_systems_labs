f0df0aec5aee09e14a54c16439dc345e965a1b28
shivan <s_ram_dhanie@hotmail.com>
2020-01-30 11:47:53 +0000

    Week1

--- Week1_-_Intro_and_setup/Week1_-_Intro_and_setup.ino
+++ Week1_-_Intro_and_setup/Week1_-_Intro_and_setup.ino
@@ -39,8 +39,5 @@ void loop() {
   Serial.println(" of fixing the serial bug");
   }*/
   
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  flash_leds();                     // wait for a second
 }
