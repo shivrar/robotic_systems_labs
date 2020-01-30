7e4f046941afe2b1e27afc02bf7131cedb943b0a
shivan <s_ram_dhanie@hotmail.com>
2020-01-30 11:50:25 +0000

    Adding common libs

--- Libraries/common_sys.h
+++ Libraries/common_sys.h
@@ -1,3 +1,11 @@
 #include <USBCore.h>    // To fix serial print behaviour bug.
 u8 USB_SendSpace(u8 ep);
 #define SERIAL_ACTIVE (USB_SendSpace(CDC_TX) >= 50)
+
+void flash_leds ()
+{
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000); 
+}
