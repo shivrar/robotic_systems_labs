cc03ea45511e44ce750ca0dc673887065fa0a50c
shivan <s_ram_dhanie@hotmail.com>
2020-02-13 23:39:00 +0000

    Initial BangBang

--- FInal_System/romi_ing/common_sys.h
+++ FInal_System/romi_ing/common_sys.h
@@ -1,4 +1,6 @@
 #include <USBCore.h>    // To fix serial print behaviour bug.
+#include "lineSensors.h"
+
 u8 USB_SendSpace(u8 ep);
 #define SERIAL_ACTIVE (USB_SendSpace(CDC_TX) >= 50)
 
@@ -26,4 +28,54 @@ void play_tone(int ledpin_no, int volume)
     analogWrite(ledpin_no, volume);
     delay(1);
     analogWrite(ledpin_no, volume);
+}
+
+void BangBang(LineSensor left, LineSensor centre, LineSensor right, int min_power, int max_power, byte &left_power, byte &right_power)
+{
+  byte sense = 0;
+  sense = sense | ( (left.readCalibrated()>200 ? 1 : 0)  << 2 );
+  sense = sense | ( (centre.readCalibrated()>200 ? 1 : 0)   << 1 );
+  sense = sense | ( (right.readCalibrated()>200 ? 1 : 0)  << 0 );
+
+  switch(sense){
+    case 0:
+      left_power = min_power;
+      right_power = min_power;
+    break;
+    case 1:
+      left_power = max_power;
+      right_power = 0;
+    break;
+    case 3:
+      left_power = max_power;
+      right_power = min_power;
+    break;
+    case 4:
+      left_power = 0;
+      right_power = max_power;
+    break;
+    case 6:
+      left_power = min_power;
+      right_power = max_power;
+    break;
+    case 7:
+      left_power = max_power;
+      right_power = max_power;
+    break;
+    default:
+    break;
+  }
+  
+}
+
+float weightedPower(LineSensor left, LineSensor centre, LineSensor right, int min_power, int max_power, byte &left_power, byte &right_power)
+{
+  float total = left.readCalibrated() + centre.readCalibrated() +right.readCalibrated();
+  float rightM = right.readCalibrated()/(total);
+  float leftM = left.readCalibrated()/(total);
+  float m = leftM - rightM;  
+
+  left_power = abs(m*max_power);
+  right_power = abs(m*max_power);
+  return m;
 }
--- FInal_System/romi_ing/romi_ing.ino
+++ FInal_System/romi_ing/romi_ing.ino
@@ -6,6 +6,8 @@
 #define LINE_LEFT_PIN A2
 #define LINE_CENTRE_PIN A3
 #define LINE_RIGHT_PIN A4
+#define FORWARD LOW
+#define REVERSE HIGH
 
 #define L_PWM_PIN 10
 #define L_DIR_PIN 16
@@ -13,7 +15,7 @@
 #define R_DIR_PIN 15
 
 int max_power = 50;
-int min_power = 12;
+int min_power = 20;
 
 float leftM, rightM, total, m;
 
@@ -39,8 +41,8 @@ void setup() {
   pinMode( R_PWM_PIN, OUTPUT );
   pinMode( R_DIR_PIN, OUTPUT );
 
-  digitalWrite( L_DIR_PIN, LOW);
-  digitalWrite( R_DIR_PIN, LOW);
+  digitalWrite( L_DIR_PIN, FORWARD);
+  digitalWrite( R_DIR_PIN, FORWARD);
   
   Serial.begin( 9600 );
 
@@ -55,23 +57,12 @@ void setup() {
   play_tone(6, 125);
   delay(1000);
   analogWrite(6, 0);
-
-//  play some beeps after calibration has run
-
   
 }
 
-//void loop() {
-//
-////N.B. e0 = left wheel, e1 = right wheel
-//
-//  Serial.print( theta_e0, 4 );
-//  Serial.print( ", ");
-//  Serial.println( theta_e1, 4 );
-//  
-//  delay( 2 );
-//}
+//TODO: Implement the probablistic nature now for the robot with confidence values.
 
+////N.B. e0 = left wheel, e1 = right wheel
 // Remmeber, loop is called again and again.
 void loop() 
 {
@@ -81,7 +72,7 @@ void loop()
 switch(state){
 //Now that we have the sensor lets try to find the line
   case 0:
-    if((l_sensor.readCalibrated() + c_sensor.readCalibrated() +r_sensor.readCalibrated())/3 < 350)
+    if((l_sensor.readCalibrated() + c_sensor.readCalibrated() +r_sensor.readCalibrated())/3 < 400)
     {
       l_power = min(l_power + 4, max_power);
       r_power = min(r_power + 4, max_power);
@@ -95,29 +86,35 @@ switch(state){
     }
   break;
   case 1:
-    total = l_sensor.readCalibrated() + c_sensor.readCalibrated() +r_sensor.readCalibrated();
-    rightM = r_sensor.readCalibrated()/(total);
-    leftM = l_sensor.readCalibrated()/(total);
-    m = leftM - rightM;
-//    Serial.print(rightM);
-//    Serial.print(",");
-//    Serial.println(leftM);
-//    Serial.println(m,5);/
+      m = weightedPower(l_sensor, c_sensor, r_sensor, min_power, max_power,l_power, r_power);
 
-    r_power = m*max_power * -1;
-    l_power = m*max_power * 1;
-    l_power = max(min(l_power, max_power), min_power);
-    r_power = max(min(r_power, max_power), min_power);
-    
-    Serial.print(m);
-    Serial.print(",");
-    Serial.print(r_power);
-    Serial.print(",");
-    Serial.println(l_power);
+      if(m >0.2)
+      {
+        digitalWrite( R_DIR_PIN, FORWARD);
+        digitalWrite( L_DIR_PIN, REVERSE);
+      }
+      else if(m < -0.2)
+      {
+        digitalWrite( R_DIR_PIN, REVERSE);
+        digitalWrite( L_DIR_PIN, FORWARD);
+      }
+      else
+      {
+        digitalWrite( L_DIR_PIN, FORWARD);
+        digitalWrite( R_DIR_PIN, FORWARD);   
+      }
+//    Serial.print(m);
+//    Serial.print(",");
+
+    l_power = max(min(l_power, max_power),min_power);
+    r_power = max(min(r_power, max_power),min_power);
+//    Serial.print(l_power);
+//    Serial.print(",");
+//    Serial.println(r_power);
     break;
 }
     // Send power PWM to pins, to motor drivers.
   analogWrite( L_PWM_PIN, l_power );
   analogWrite( R_PWM_PIN, r_power );
-  delay(20);
+  delay(10);
 }
