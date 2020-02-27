12a246882100a6097ac47f7f4012412aa6ab0ede
shivan <s_ram_dhanie@hotmail.com>
2020-02-27 00:08:05 +0000

    RTH implemented

--- FInal_System/romi_ing/romi_ing.ino
+++ FInal_System/romi_ing/romi_ing.ino
@@ -32,7 +32,7 @@ float slope = 0.1056, y_int = 0.287;
 PID left_wheel(0.3, 0.0000,1.0);
 PID right_wheel(0.3, 0.0000,1.0);
 PID heading(0.24,0.0,0.9);
-PID rth_heading(0.1,0.0,0.0);
+PID rth_heading(0.2,0.0,0.0);
 
 
 volatile byte l_power;
@@ -200,6 +200,7 @@ switch(state){
     break;
     
     }
+    
     case -3:
     {
     /*Drive forward a bit so we can figure out if the kinematics are working alright*/
@@ -224,31 +225,29 @@ switch(state){
     /*Return to home - 
       First lets look at home
     */
-      if( elapsed_time >= 50 )
+      if( elapsed_time >= 40 )
       {
-        float alpha = acos((Romi.getPose().x*cos(Romi.getPose().theta) + Romi.getPose().y*sin(Romi.getPose().theta))/sqrt(square(Romi.getPose().x) + square(Romi.getPose().y)));
-        float home_heading = (Romi.getPose().theta>=0 && Romi.getPose().theta<=M_PI) ? M_PI - alpha : alpha - M_PI;
-        float rth_heading_output = 0.0;
         float right_output = 0.0;
         float left_output = 0.0;
-        last_timestamp = millis();    
-        rth_heading_output = rth_heading.update(0.0, home_heading);
+        float alpha = acos((Romi.getPose().x*cos(Romi.getPose().theta) + Romi.getPose().y*sin(Romi.getPose().theta))/sqrt(square(Romi.getPose().x) + square(Romi.getPose().y)));
+        float home_heading = (Romi.getPose().theta>=0 && Romi.getPose().theta<=M_PI) ? M_PI - alpha : alpha - M_PI;
+        float rth_heading_output = rth_heading.update(0.0, home_heading);
+        float abs_distance = sqrt(square(Romi.getPose().x) + square(Romi.getPose().y));
+        last_timestamp = millis();   
         count++;
         if(count%2==0)
         {
           right_output = right_wheel.update(-rth_heading_output/elapsed_time, right_wheel_est);
           left_output = left_wheel.update(rth_heading_output/elapsed_time, left_wheel_est);
           count = 0;
-          l_power = max(abs((left_output - y_int)/slope), min_power);
-          r_power = max(abs((right_output - y_int)/slope), min_power);
-          if(home_heading > 0.1 || home_heading < -0.1)
+          if(home_heading > 0.0523599 || home_heading < -0.0523599)
           {
-            if(rth_heading_output >0.1)
+            if(rth_heading_output >0.0523599)
             {
               r_direction = REVERSE;
               l_direction = FORWARD;
               }
-            else if(rth_heading_output < -0.1)
+            else if(rth_heading_output < -0.0523599)
             {
               r_direction = FORWARD;
               l_direction = REVERSE;
@@ -263,76 +262,71 @@ switch(state){
               state = 3;
           }
         }
-        Serial.print("home_heading");
-        Serial.print(",");
-//        Serial.println("theta");
-//        Serial.print(",");
-        Serial.println("rth_heading_output");
-        Serial.print(home_heading);
-        Serial.print(",");
-//        Serial.print(Romi.getPose().theta);
-//        Serial.print(",");
-        Serial.println(rth_heading_output);
+        l_power = max(abs((left_output - y_int)/slope), min_power);
+        r_power = max(abs((right_output - y_int)/slope), min_power);
       }
     }
     break;
     case 3:
     {
-      if( elapsed_time >= 50 )
+      if( elapsed_time >= 25 )
       {
-        float alpha = acos((Romi.getPose().x*cos(Romi.getPose().theta) + Romi.getPose().y*sin(Romi.getPose().theta))/sqrt(square(Romi.getPose().x) + square(Romi.getPose().y)));
-        float home_heading = (Romi.getPose().theta>=0 && Romi.getPose().theta<=M_PI) ? M_PI - alpha : alpha - M_PI;
-        float rth_heading_output = 0.0;
         float right_output = 0.0;
         float left_output = 0.0;
+        float alpha = acos((Romi.getPose().x*cos(Romi.getPose().theta) + Romi.getPose().y*sin(Romi.getPose().theta))/sqrt(square(Romi.getPose().x) + square(Romi.getPose().y)));
+        float home_heading = (Romi.getPose().theta>=0 && Romi.getPose().theta<=M_PI) ? M_PI - alpha : alpha - M_PI;
+        float rth_heading_output = rth_heading.update(0.0, home_heading);
+        float abs_distance = sqrt(square(Romi.getPose().x) + square(Romi.getPose().y));
         last_timestamp = millis();    
-        rth_heading_output = rth_heading.update(0.0, home_heading);
         count++;
         if(count%2==0)
         {
           right_output = right_wheel.update(-rth_heading_output/elapsed_time, right_wheel_est);
           left_output = left_wheel.update(rth_heading_output/elapsed_time, left_wheel_est);
           count = 0;
-          if(rth_heading_output >0.1)
+          if(abs_distance > 0.015)
           {
-            r_direction = REVERSE;
-            l_direction = FORWARD;
+            if(rth_heading_output >0.0698132)
+            {
+              r_direction = REVERSE;
+              l_direction = FORWARD;
             }
-          else if(rth_heading_output < -0.1)
-          {
-            r_direction = FORWARD;
-            l_direction = REVERSE;
-          }
-          else if(home_heading < 0.1 && home_heading > -0.1)
-          {
-              l_power = 0.0;
-              r_power = 0.0;
+            else if(rth_heading_output < -0.0698132)
+            {
+              r_direction = FORWARD;
+              l_direction = REVERSE;
+            }
+            else
+            {
               r_direction = FORWARD;
               l_direction = FORWARD;
+              right_output = max_des_speed/2;
+              left_output = max_des_speed/2;
+            }
           }
           else
           {
-            r_direction = FORWARD;
-            l_direction = FORWARD;
-            right_output = max_des_speed/2;
-            left_output= max_des_speed/2;
+              r_direction = FORWARD;
+              l_direction = FORWARD;
+              right_output = 0.0;
+              left_output = 0.0;
+              state = 4;  
           }
         }
-//        Serial.print("home_heading");
-//        Serial.print(",");
-//        Serial.print("theta");
-//        Serial.print(",");
-//        Serial.println("rth_heading_output");
-//        Serial.print(home_heading);
-//        Serial.print(",");
-//        Serial.print(Romi.getPose().theta);
-//        Serial.print(",");
-//        Serial.println(rth_heading_output);
         l_power = max(abs((left_output - y_int)/slope), min_power);
         r_power = max(abs((right_output - y_int)/slope), min_power);
+        Serial.println("Abs_distance,");
+        Serial.print(abs_distance);
       }
     }
     break;
+    case 4:
+    {
+        l_power = 0.0;
+        r_power = 0.0;
+        // Finished
+        break;
+    }
 }
   // Send power PWM to pins, to motor drivers.
   digitalWrite( R_DIR_PIN, r_direction);
